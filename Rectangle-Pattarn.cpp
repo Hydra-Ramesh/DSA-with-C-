eb5dc3ea6b1c1af4a874c7d3604c55bd2b8a6334
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-01-30 19:04:32 +0530

    All Questions from Basic Pattern Printing

---
+++ Rectangle-Pattarn.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+   cout<<"Enter the rows " ;
+   cin>>n;
+   cout<<"Enter the number of cols ";
+   cin>>m;
+   for(int i=1;i<=n;i++){
+    for(int j=1;j<=m;j++){
+        cout<<"* ";
+    }cout<<endl;
+   }
+   return 0;
+}
