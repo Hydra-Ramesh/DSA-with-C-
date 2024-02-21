2ec9b77ea320ab967cdbbdc6bd7750c7baa92c62
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-21 23:45:33 +0530

    Basic Concept of Vector

---
+++ Capacity-of-vector.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+     vector<int>v;
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(10);
+    v.pop_back();
+    cout<<v.capacity();
+    cout<<endl;
+    cout<<v.size();
+}
